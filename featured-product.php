e546cfa9d92afe116ceb3434a2cc2b5698f2b66b
Kaushik Acharya <kaushik@palebluedot.net>
2017-01-11 16:41:17 -0800

    Got the basics set up -  Basic scaffolding -  Data dir with example CSV file Next up: -  Add usable data -  Read CSV input

--- featured-product.php
+++ featured-product.php
@@ -12,31 +12,43 @@ Text Domain: featured-product
 */
 
 
-/* Housekeeping functions */
+// Housekeeping functions
 
-function pluginprefix_setup_post_types()
+function featured-product_setup_post_types()
 {
+        //FIXME:  Figure out what post types is / are required for this.  Doesn't
+        //        look like we need any for this plugin
         // register the "book" custom post type
-            register_post_type( 'book', ['public' => 'true'] );
+        //register_post_type( 'book', ['public' => 'true'] );
 }
-add_action( 'init', 'pluginprefix_setup_post_type' );
+add_action( 'init', 'featured-product_setup_post_type' );
  
- function pluginprefix_install()
+ function featured-product_install()
  {
          // trigger our function that registers the custom post type
-             pluginprefix_setup_post_types();
+             featured-product_setup_post_types();
               
                   // clear the permalinks after the post type has been registered
                       flush_rewrite_rules();
  }
- register_activation_hook( __FILE__, 'pluginprefix_install' );
+ register_activation_hook( __FILE__, 'featured-product_install' );
 
 
-function pluginprefix_deactivation()
+function featured-product_deactivation()
 {
         // our post type will be automatically removed, so no need to unregister it
          
-             // clear the permalinks to remove our post type's rules
-                 flush_rewrite_rules();
+        // clear the permalinks to remove our post type's rules
+        flush_rewrite_rules();
 }
-register_deactivation_hook( __FILE__, 'pluginprefix_deactivation' );
+register_deactivation_hook( __FILE__, 'featured-product_deactivation' );
+
+
+// This is where we remove data files, drop DB tables, etc as needed.
+// For now, just aliasing this to the deactivation function
+// FIXME:  Revisit this before production launch
+register_uninstall_hook(__FILE__, 'featured-product_deactivate');
+
+// TODO:  Read CSV input file
+
+// TODO:  Create HTML snippet containing image, store links and corresponding prices
