819f9eb772d636c7513b1ff2417689fe78afc32c
Kaushik Acharya <kaushik@palebluedot.net>
2017-02-09 17:03:17 -0800

    Rewritten as a plugin that provides a widget Rookie mistake, started writing a plugin when I needed a widget. -  Placement works correctly -  Image works -  Got a stupidly simple "smart" href working that names the link based    on the shop domain
    Next up:
    -  Switch to storing data in DB instead of CSV file
    -  Related to the above, provide options to update widget from widget
       admin

--- featured-product.php
+++ featured-product.php
@@ -11,69 +11,73 @@ License URI: https://www.gnu.org/licenses/gpl-2.0.html
 Text Domain: featured_product
 */
 
-// Get the header out of the way
-header('Content-Type: text/html');
+class kacharya_featured_product extends WP_Widget {
 
-// Housekeeping functions
+    public function __construct() {
+        $widget_options = array( 
+            'classname' => 'featured_product',
+            'description' => 'Featured product rotator.  Supports multiple links per product.',
+        );
+            parent::__construct( 'featured_product', 'Featured Product', $widget_options );
+    }
 
-function featured_product_setup_post_types()
-{
-        //FIXME:  Figure out what post types is / are required for this.  Doesn't
-        //        look like we need any for this plugin
-        // register the "book" custom post type
-        //register_post_type( 'book', ['public' => 'true'] );
-}
-add_action( 'init', 'featured_product_setup_post_type' );
- 
- function featured_product_install()
- {
-         // trigger our function that registers the custom post type
-             featured_product_setup_post_types();
-              
-                  // clear the permalinks after the post type has been registered
-                      flush_rewrite_rules();
- }
- register_activation_hook( __FILE__, 'featured_product_install' );
+    public function widget ($args, $instance) {
 
+        // Read CSV input file and split each line into array
+        // NOTE:  File location relative to plugin dir works fine in PHP,
+        // but under Wordpress, the absolute path seems to be required
+        $data_file = __DIR__ . "/data/featured-products.csv";
+        
+        $handle = fopen( $data_file, "r" ) or exit( "Unable to open data file " . $data_file );
+        
+        echo '<aside id="featured-product" class="widget featured_product">' . "\n";
+        
+        while ( !feof($handle) ) {
+            $line     = fgets( $handle );
+            $elements = explode( ",", $line );
+            $array    = new ArrayObject( $elements );
+            $iter     = $array->getIterator();
+        
+            // The first element is the image URL
+            if ( $iter->valid() ) {
 
-function featured_product_deactivation()
-{
-        // our post type will be automatically removed, so no need to unregister it
-         
-        // clear the permalinks to remove our post type's rules
-        flush_rewrite_rules();
-}
-register_deactivation_hook( __FILE__, 'featured_product_deactivation' );
+                echo '<div class="featured-product"><img src="' . $iter->current() . '"/></div>' . "\n";
+                $iter->next();
+            }
 
+            while ( $iter->valid() ) {
 
-// This is where we remove data files, drop DB tables, etc as needed.
-// For now, just aliasing this to the deactivation function
-// FIXME:  Revisit this before production launch
-register_uninstall_hook(__FILE__, 'featured_product_deactivate');
+                $current = $iter->current();
 
-// Read CSV input file and split each line into array
-// NOTE:  File location relative to plugin dir works fine in PHP,
-// but under Wordpress, the absolute path seems to be required
-$data_file = __DIR__ . "/data/featured-products.csv";
+                if ( preg_match('/craftsy/', $current) ) {
 
-$handle = fopen( $data_file, "r" ) or exit( "Unable to open data file " . $data_file );
+                    echo '<div class="featured-product"><a href="' . $current . '" target="_blank">Craftsy</a></div>' . "\n";
+                }
+                elseif ( preg_match('/etsy/', $current) ) {
 
-echo '<aside id="featured-product" class="widget featured_product">' . "\n";
+                    echo '<div class="featured-product"><a href="' . $current . '" target="_blank">Etsy</a></div>' . "\n";
+                }
+                else {
 
-while ( !feof($handle) ) {
-    $line     = fgets( $handle );
-    $elements = explode( ",", $line );
-    $array    = new ArrayObject( $elements );
-    $iter     = $array->getIterator();
+                    echo '<div class="featured-product"><h4>$' . $current . '</h4></div>' . "\n";
+                }
 
-    while ( $iter->valid() ) {
-        echo '<div class="featured-product"><h3>' . $iter->current() . '</h3></div>' . "\n";
-        $iter->next();
+                $iter->next();
+            }
+        }
+        
+        echo '</aside>' . "\n";
+        
+        fclose($handle);
+        
+        // TODO:  Create HTML snippet containing image, store links and corresponding prices
     }
 }
 
-echo '</aside>' . "\n";
+function kacharya_register_featured_product() {
 
-fclose($handle);
+    register_widget( 'kacharya_featured_product' );
+}
 
-// TODO:  Create HTML snippet containing image, store links and corresponding prices
+add_action( 'widgets_init', 'kacharya_register_featured_product' );
+
