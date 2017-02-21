208d3c087716f3a47612a027aa4d407a53bf3046
Kaushik Acharya <kaushik@palebluedot.net>
2017-02-20 18:15:24 -0800

    First working version, updated Readme -  Changes: -  Added widget title -  Added shop URL and link text -  Fixed minor styling

--- README.md
+++ README.md
@@ -1,14 +1,18 @@
 # featured-product
-A Wordpress plugin to rotate featured products / events.  Supports two store-specific links per featured item.
+A Wordpress plugin that provides a widget to randomly rotate featured items.  Supports two store-specific links per featured item.
 The idea is that you can have the same product listed at different stores (e.g. Etsy and Craftsy) and
 wish to give the customer a choice of stores to buy from.
 
+Also supports a link to your Shop Page.
+
 ## Installation:
-FIXME
+Simply copy the `featured-product.php' file to the `plugins` directory of your WordPress installation
 
 ## Data:
--  Uses widget admin for item entry, user-configurable number of items
+-  Uses widget admin for all user-configurable items
 -  Required Fields:
+    -  Widget Title
+    -  Number of itmes (default: 5)
     -  Image URL (thumbnail image recommended)
     -  Item name
     -  Item Price
@@ -18,3 +22,7 @@ FIXME
         -  OR
         -  Store2 URL
         -  Link Text 2
+
+-  Optional Fields:
+    -  Shop Page URL
+    -  Shop Link Text
--- featured-product.php
+++ featured-product.php
@@ -24,10 +24,23 @@ class kacharya_featured_product extends WP_Widget {
     public function widget ($args, $instance) {
 
         echo '<aside id="featured-product" class="widget featured_product">' . "\n";
+        echo '<h3 class="widget-title">' . $instance['title'] . "</h3>\n";
 
-        $how_many = !empty( $instance['how_many'] ) ? $instance['how_many'] : 0;
+        if ( !empty($instance['shop_url']) && !empty($instance['shop_link_text']) ) {
+        
+            echo '<strong><a href="' . $instance['shop_url'] . '" target="_blank">' . $instance['shop_link_text'] . "</a></strong>\n";
+        }
 
-        for ($i = 0; $i < $how_many; $i++) {
+        $how_many_items = !empty( $instance['how_many_items'] ) ? $instance['how_many_items'] : 0;
+
+        // How many random items to pick
+        $how_many_to_display = 2;
+
+        // Based on how many items there are total, build an array of that many indices,
+        // then pick N random ones (defined above) from that array
+        $indices_to_display = array_rand( range(0, ($how_many_items - 1)), $how_many_to_display );
+
+        foreach ( $indices_to_display as &$i ) {
 
             // Only attempt to display an item if all required fields are present:
             // -  image URL
@@ -38,7 +51,8 @@ class kacharya_featured_product extends WP_Widget {
                 && !empty($instance["price_$i"]) && (!empty($instance["store1_link_$i"]) || (!empty($instance["store2_link_$i"]))) ) {
 
                 // Image URL
-                echo '<div class="featured-product"><img src="' . $instance["image_url_$i"] . '"/></div>' . "\n";
+                echo '<div class="featured-product" style="padding-top: 20px;"><img src="' . $instance["image_url_$i"] 
+                    . '"/></div>' . "\n<br>\n";
 
                 // Item name
                 echo '<div class="featured-product">' . $instance["item_name_$i"] . '</div>' . "\n";
@@ -65,57 +79,77 @@ class kacharya_featured_product extends WP_Widget {
             }
         }
 
+        unset( $i );
+
         echo '</aside>' . "\n";
     }
 
 	public function form( $instance ) {
 
-		$how_many = !empty( $instance['how_many'] ) ? $instance['how_many'] : 5; ?>
+		$title = !empty( $instance['title'] ) ? $instance['title'] : 'Featured Patterns'; ?>
 		<p>
-			<label for="<?php echo $this->get_field_id( 'how_many' ); ?>">Number of items:</label>
-			<input type="text" id="<?php echo $this->get_field_id( 'how_many' ); ?>" name="<?php echo $this->get_field_name( 'how_many' ); ?>" value="<?php echo esc_attr( $how_many ); ?>" />
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>">Title:</label>
+			<input type="text" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo esc_attr( $title ); ?>" />
+	    </p><?php
+
+		$shop_url = $instance['shop_url']; ?>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'shop_url' ); ?>">Shop Page URL:</label>
+			<input type="text" id="<?php echo $this->get_field_id( 'shop_url' ); ?>" name="<?php echo $this->get_field_name( 'shop_url' ); ?>" value="<?php echo esc_attr( $shop_url ); ?>" />
+	    </p><?php
+
+		$shop_link_text = $instance['shop_link_text']; ?>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'shop_link_text' ); ?>">Shop Page Link Text:</label>
+			<input type="text" id="<?php echo $this->get_field_id( 'shop_link_text' ); ?>" name="<?php echo $this->get_field_name( 'shop_link_text' ); ?>" value="<?php echo esc_attr( $shop_link_text ); ?>" />
+	    </p><?php
+
+		$how_many_items = !empty( $instance['how_many_items'] ) ? $instance['how_many_items'] : 5; ?>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'how_many_items' ); ?>">Number of items:</label>
+			<input type="text" id="<?php echo $this->get_field_id( 'how_many_items' ); ?>" name="<?php echo $this->get_field_name( 'how_many_items' ); ?>" value="<?php echo esc_attr( $how_many_items ); ?>" />
 	    </p>
         <hr><?php
 
-        for ($i = 0; $i < $how_many; $i++) {
+        for ($i = 0; $i < $how_many_items; $i++) {
 
-            $image_url[$i] = !empty( $instance["image_url_$i"] ) ? $instance["image_url_$i"] : ''; ?>
+            $image_url[$i] = $instance["image_url_$i"]; ?>
             <p>
                 <label for="<?php echo $this->get_field_id( "image_url_$i" ); ?>">Image URL:</label>
                 <input type="text" id="<?php echo $this->get_field_id( "image_url_$i" ); ?>" name="<?php echo $this->get_field_name( "image_url_$i" ); ?>" value="<?php echo esc_attr( $image_url[$i] ); ?>" />
             </p><?php
 
-            $item_name[$i] = !empty( $instance["item_name_$i"] ) ? $instance["item_name_$i"] : ''; ?>
+            $item_name[$i] = $instance["item_name_$i"]; ?>
             <p>
                 <label for="<?php echo $this->get_field_id( "item_name_$i" ); ?>">Item Name:</label>
                 <input type="text" id="<?php echo $this->get_field_id( "item_name_$i" ); ?>" name="<?php echo $this->get_field_name( "item_name_$i" ); ?>" value="<?php echo esc_attr( $item_name[$i] ); ?>" />
             </p><?php
 
-            $price[$i] = !empty( $instance["price_$i"] ) ? $instance["price_$i"] : ''; ?>
+            $price[$i] = $instance["price_$i"]; ?>
             <p>
                 <label for="<?php echo $this->get_field_id( "price_$i" ); ?>">Price:</label>
                 <input type="text" id="<?php echo $this->get_field_id( "price_$i" ); ?>" name="<?php echo $this->get_field_name( "price_$i" ); ?>" value="<?php echo esc_attr( $price[$i] ); ?>" />
             </p><?php
 
-            $store1_link[$i] = !empty( $instance["store1_link_$i"] ) ? $instance["store1_link_$i"] : ''; ?>
+            $store1_link[$i] = $instance["store1_link_$i"]; ?>
             <p>
                 <label for="<?php echo $this->get_field_id( "store1_link_$i" ); ?>">Store 1 Link:</label>
                 <input type="text" id="<?php echo $this->get_field_id( "store1_link_$i" ); ?>" name="<?php echo $this->get_field_name( "store1_link_$i" ); ?>" value="<?php echo esc_attr( $store1_link[$i] ); ?>" />
             </p><?php
 
-            $link1_label[$i] = !empty( $instance["link1_label_$i"] ) ? $instance["link1_label_$i"] : ''; ?>
+            $link1_label[$i] = $instance["link1_label_$i"]; ?>
             <p>
                 <label for="<?php echo $this->get_field_id( "link1_label_$i" ); ?>">Store 1 Link Text:</label>
                 <input type="text" id="<?php echo $this->get_field_id( "link1_label_$i" ); ?>" name="<?php echo $this->get_field_name( "link1_label_$i" ); ?>" value="<?php echo esc_attr( $link1_label[$i] ); ?>" />
             </p><?php
 
-            $store2_link[$i] = !empty( $instance["store2_link_$i"] ) ? $instance["store2_link_$i"] : ''; ?>
+            $store2_link[$i] = $instance["store2_link_$i"]; ?>
             <p>
                 <label for="<?php echo $this->get_field_id( "store2_link_$i" ); ?>">Store 2 Link:</label>
                 <input type="text" id="<?php echo $this->get_field_id( "store2_link_$i" ); ?>" name="<?php echo $this->get_field_name( "store2_link_$i" ); ?>" value="<?php echo esc_attr( $store2_link[$i] ); ?>" />
             </p><?php
 
-            $link2_label[$i] = !empty( $instance["link2_label_$i"] ) ? $instance["link2_label_$i"] : ''; ?>
+            $link2_label[$i] = $instance["link2_label_$i"]; ?>
             <p>
                 <label for="<?php echo $this->get_field_id( "link2_label_$i" ); ?>">Store 2 Link Text:</label>
                 <input type="text" id="<?php echo $this->get_field_id( "link2_label_$i" ); ?>" name="<?php echo $this->get_field_name( "link2_label_$i" ); ?>" value="<?php echo esc_attr( $link2_label[$i] ); ?>" />
